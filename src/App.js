578b2cf0335ff9bddaa0ffc20e1f531b1db6206b
arturovilla <acvturi@gmail.com>
2022-08-02 14:36:11 -0500

    added home page with new routing for each clothing catagory, also added product card component such that only they need to be re rendered each time a new catagory is picked

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Adminsignin from "./pages/Adminsignin";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Productcards from "./components/Productcards";
 //
 function App() {
 	return (
@@ -14,7 +16,56 @@ function App() {
 					<Route path="/" element={<Login />} />
 					<Route path="/signup" element={<Signup />} />
 					<Route path="/admin-sign-in" element={<Adminsignin />} />
-					<Route path="/home" element={<Home />} />
+					<Route
+						path="/home"
+						element={
+							<>
+								<Home />
+								<Navbar />
+								<Productcards />
+							</>
+						}
+					/>
+					<Route
+						path="/menswear"
+						element={
+							<>
+								<Home /> <Navbar />
+							</>
+						}
+					/>
+					<Route
+						path="/womenswear"
+						element={
+							<>
+								<Home /> <Navbar />
+							</>
+						}
+					/>
+					<Route
+						path="/tops"
+						element={
+							<>
+								<Home /> <Navbar />
+							</>
+						}
+					/>
+					<Route
+						path="/bottoms"
+						element={
+							<>
+								<Home /> <Navbar />
+							</>
+						}
+					/>
+					<Route
+						path="/accessories"
+						element={
+							<>
+								<Home /> <Navbar />
+							</>
+						}
+					/>
 				</Routes>
 			</BrowserRouter>
 		</div>
